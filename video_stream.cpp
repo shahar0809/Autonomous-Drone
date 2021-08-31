de97bcff0d81e8ad8220ba6e41c8b94063f4f633
shahar0809 <shahartefler@gmail.com>
2021-08-31 20:27:57 +0300

    Add camera calibration

--- video_stream.cpp
+++ video_stream.cpp
@@ -3,6 +3,7 @@
 #include <mutex>
 #include <System.h>
 #include "ctello.h"
+#include "camera_calibration.cpp"
 #include <opencv2/core/core.hpp>
 #include <opencv2/opencv.hpp>
 
@@ -12,21 +13,25 @@ using cv::imshow;
 using cv::VideoCapture;
 using cv::waitKey;
 
-void init_tello(Tello& tello);
-
-[[noreturn]] void video_stream(bool& isDone, std::mutex& doneMutex, cv::VideoCapture& capture, ORB_SLAM2::System& SLAM);
+void video_stream(bool& isDone, std::mutex& doneMutex, cv::VideoCapture& capture,
+                  ORB_SLAM2::System& SLAM, Calibration& cal);
 
 const char* const TELLO_STREAM_URL { "udp://0.0.0.0:11111?overrun_nonfatal=1&fifo_size=50000000" };
 
 // ORB-SLAM configs
-const char* const PATH_TO_VOCABULARY { "ORB_SLAM2/Vocabulary/ORBvoc.txt" };
-const char* const PATH_TO_CONFIG { "/home/magshimim/Documents/exit-scan/ORB_SLAM2/Examples/Monocular/TUM1.yaml" };
+const char* const PATH_TO_VOCABULARY { "/home/magshimim/Documents/exit-scan/ORB_SLAM2/Vocabulary/ORBvoc.txt" };
+const char* const PATH_TO_CONFIG { "/home/magshimim/Documents/exit-scan/TUM1.yaml" };
+
+const int CIRCLE_DEGREES = 360;
+const int TURN_DEGREES = 15;
 
 int main()
 {
     ctello::Tello tello;
     bool isDone =  false;
     std::mutex doneMutex;
+
+    Calibration cal = calibrate_camera("out_camera_data.yaml");
 
     // Initializing ORB-SLAM2
     ORB_SLAM2::System SLAM(PATH_TO_VOCABULARY, PATH_TO_CONFIG, ORB_SLAM2::System::MONOCULAR, true);
@@ -55,13 +60,31 @@ int main()
     // Initializing OpenCV video stream from Tello camera
     cv::VideoCapture capture{TELLO_STREAM_URL, CAP_FFMPEG};
 
-    std::thread video(video_stream, std::ref(isDone), std::ref(doneMutex), std::ref(capture), std::ref(SLAM));
+    std::thread video(video_stream, std::ref(isDone), std::ref(doneMutex), std::ref(capture),
+                      std::ref(SLAM), std::ref(cal));
 
     // Scan the room 360 degrees
     //tello.SendCommand("cw 360");
     //while (!(tello.ReceiveResponse()));
 
-    sleep(30);
+    while (true)
+    {
+        for (int i = 0; i < CIRCLE_DEGREES; i += TURN_DEGREES)
+        {
+            //tello.SendCommand("forward 2");
+            //while (!(tello.ReceiveResponse()));
+            //tello.SendCommand("back 2");
+            //while (!(tello.ReceiveResponse()));
+            tello.SendCommand("cw " + std::to_string(TURN_DEGREES));
+            while (!(tello.ReceiveResponse()));
+            sleep(3);
+
+            if (SLAM.GetTrackingState())
+        }
+    }
+
+
+    //sleep(30);
 
     // Telling video stream thread to terminate
     doneMutex.lock();
@@ -86,7 +109,8 @@ void init_tello(Tello& tello)
     while (!(tello.ReceiveResponse()));
 }
 
-void video_stream(bool& isDone, std::mutex& doneMutex, cv::VideoCapture& capture, ORB_SLAM2::System& SLAM)
+void video_stream(bool& isDone, std::mutex& doneMutex, cv::VideoCapture& capture,
+                  ORB_SLAM2::System& SLAM, Calibration& cal)
 {
     std::cout << "stream" << std::endl;
 
@@ -101,6 +125,8 @@ void video_stream(bool& isDone, std::mutex& doneMutex, cv::VideoCapture& capture
         // Get frames from Tello video stream
         cv::Mat frame;
         capture >> frame;
+
+        undistort(frame, frame, cal.cameraMatrix, cal.distCoeffs);
 
         // Pass the image to the SLAM system
         SLAM.TrackMonocular(frame, 0.2);
