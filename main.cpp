be68d2ea80542114e597d414c328433ed6966820
DianaSK <87084600+DianaSK@users.noreply.github.com>
2021-09-15 23:56:55 +0300

    how to find the angle (navigation)

--- main.cpp
+++ main.cpp
@@ -1,23 +1,13 @@
-#include <iostream>#include <iostream>
-#include "FindExit.h"
-#include "FindPath.h"
+
 
 int main() {
 
-    //get position
-    /*
-    cv::Mat Tcw = SLAM.TrackMonocular(frame, 0.2);
-    cv::Mat Rwc = Tcw.rowRange(0,3).colRange(0,3).t(); // Rotation information
-    cv::Mat twc = -Rwc*Tcw.rowRange(0,3).col(3); // translation information
-    pos = Point(twc.at(0,0),twc.at(2,0));
-    double z=twc.at(1,0)
-     */
+    navigator.set_exit_point(FindExit());
 
-    Point exit;
-    exit = FindExit();
-    std::cout << "The Exit point is: " << exit.getx() <<","<< exit.gety() <<std::endl;
-    gotoExit(exit, Point(0,0), 3);
-    // gotoExit(exit, pos, z);
+    float angle = navigator.calc_rotation_angle();
+
+    tello.SendCommand("cw " + std::to_string(angle));
+    while (!(tello.ReceiveResponse()));
 
 
     return 0;
