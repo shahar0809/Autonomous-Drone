7f5b6807bd5c8b5f118753b792e8477fd990b20d
DianaSK <87084600+DianaSK@users.noreply.github.com>
2021-09-16 16:20:15 +0300

    Update dist_point.cpp

--- ORB_SLAM2/Examples/Monocular/dist_point.cpp
+++ ORB_SLAM2/Examples/Monocular/dist_point.cpp
@@ -1,28 +1,43 @@
 #include "dist_point.hpp"
 
-Dispoint::Dispoint(){
-    point.setX(0);
-    point.setY(0);
+Dispoint::Dispoint()
+{
+    point.set_x(0);
+    point.set_y(0);
 }
-void Dispoint::add(){
+
+void Dispoint::add()
+{
     num++;
 }
 
-Dispoint::Dispoint (Point _point){
-    point.setX(_point.getX());
-    point.setY(_point.getY());
+Dispoint::Dispoint (Point _point)
+{
+    point.set_x(_point.get_x());
+    point.set_y(_point.get_y());
 }
 
-void Dispoint::setdis(std::vector<Point> &vertex){
-    if(point.getX()>=vertex[0].getX()&&point.getX()<=vertex[1].getX()){
-        dis=std::min(abs(point.getY()-vertex[0].getY()),abs(point.getY()-vertex[3].getY()));
-    }else if(point.getY()>=vertex[3].getY()&&point.getY()<=vertex[0].getY()){
-        dis=std::min(abs(point.getX()-vertex[0].getX()),abs(point.getX()-vertex[2].getX()));
-    }else{
-        dis=std::min(std::min(sqrt(pow(point.getX()-vertex[0].getX(), 2) + pow(point.getY()-vertex[0].getY(), 2)),
-                              sqrt(pow(point.getX()-vertex[1].getX(), 2) + pow(point.getY()-vertex[1].getY(), 2))),
-                     std:: min(sqrt(pow(point.getX()-vertex[2].getX(), 2) + pow(point.getY()-vertex[2].getY(), 2)),
-                               sqrt(pow(point.getX()-vertex[3].getX(), 2) + pow(point.getY()-vertex[3].getY(), 2))));
+void Dispoint::set_dist(std::vector<Point> &vertex)
+{
+    if (point.get_x() >= vertex[0].get_x() && 
+    point.get_x() <= vertex[1].get_x())
+    {
+        dis = std::min(abs(point.get_y() - vertex[0].get_y()),
+        abs(point.get_y() - vertex[3].get_y()));
+    }
+    else if (point.get_y() >= vertex[3].get_y() &&
+    point.get_y() <= vertex[0].get_y())
+    {
+        dis = std::min(abs(point.get_x() - vertex[0].get_x()), 
+        abs(point.get_x() - vertex[2].get_x()));
+    }
+    else
+    {
+        dis = std::min(std::min(sqrt(pow(point.get_x() - vertex[0].get_x(), 2) 
+        + pow(point.get_y()-vertex[0].get_y(), 2)),
+                              sqrt(pow(point.get_x()-vertex[1].get_x(), 2) + pow(point.get_y()-vertex[1].get_y(), 2))),
+                     std:: min(sqrt(pow(point.get_x()-vertex[2].get_x(), 2) + pow(point.get_y()-vertex[2].get_y(), 2)),
+                               sqrt(pow(point.get_x()-vertex[3].get_x(), 2) + pow(point.get_y()-vertex[3].get_y(), 2))));
     }
 }
 bool operator<(const Dispoint& l, const Dispoint& r)
